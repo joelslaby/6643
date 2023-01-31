3f1be4a9fbf1f9456a9c02331a94a568800336ae
joelslaby <joelslaby@gatech.edu>
2023-01-31 14:53:02 -0500

    hw2 init

--- HW2_code/HW2_driver.jl
+++ HW2_code/HW2_driver.jl
@@ -12,7 +12,7 @@ include("HW2_your_code.jl")
 ########################################
 # Change size_list to choose matrix 
 # sizes suitable for your system
-size_list = 2 .^ (6 : 12)
+size_list = 2 .^ (4)
 ########################################
 time_kernel = Float64[]
 time_BLAS = Float64[]
@@ -34,77 +34,77 @@ save("performance_per_size.pdf", pl_dense)
 #----------------------------------------
 
 
-#----------------------------------------
-# Problem b
-#----------------------------------------
-# Testing blocked matmul function
-A = zeros(2014, 301)
-B = randn(2014, 1037)
-C = randn(1037, 301)
-println("Testing for memory allocation of blocked matmul...")
-allocated_memory = @ballocated add_to_A_B_times_C!(A, B, C, 301)
-@assert allocated_memory == 0
-println("No memory allocated, good!")
-println("Testing correctness of blocked matmul...")
-A .= 0; add_to_A_B_times_C!(A, B, C, 301)
-@assert A ≈ B * C
-println("Result correct, good!")
-#----------------------------------------
+# #----------------------------------------
+# # Problem b
+# #----------------------------------------
+# # Testing blocked matmul function
+# A = zeros(2014, 301)
+# B = randn(2014, 1037)
+# C = randn(1037, 301)
+# println("Testing for memory allocation of blocked matmul...")
+# allocated_memory = @ballocated add_to_A_B_times_C!(A, B, C, 301)
+# @assert allocated_memory == 0
+# println("No memory allocated, good!")
+# println("Testing correctness of blocked matmul...")
+# A .= 0; add_to_A_B_times_C!(A, B, C, 301)
+# @assert A ≈ B * C
+# println("Result correct, good!")
+# #----------------------------------------
 
 
-#----------------------------------------
-# Problem c
-#----------------------------------------
-########################################
-# Change size_list to choose matrix 
-# sizes suitable for your system
-l = 6010
-m = 6011
-n = 6070
-# Modify 
-# sizes suitable for your system
-block_sizes = 2 .^ (2 : 12)
-########################################
-time_blocked = Float64[]
-for bks in block_sizes
-    A = zeros(l, n)
-    B = randn(l, m)
-    C = randn(m, n)
-    push!(time_blocked, @elapsed add_to_A_B_times_C!(A, B, C, bks))
-end
-pl_blocked = scatter(block_sizes, time_blocked, label="blocked", axis=(xlabel="block size", ylabel="time", yscale=log10))
-axislegend(position=:lt)
-save("performance_blocked.pdf", pl_blocked)
-#----------------------------------------
+# #----------------------------------------
+# # Problem c
+# #----------------------------------------
+# ########################################
+# # Change size_list to choose matrix 
+# # sizes suitable for your system
+# l = 6010
+# m = 6011
+# n = 6070
+# # Modify 
+# # sizes suitable for your system
+# block_sizes = 2 .^ (2 : 12)
+# ########################################
+# time_blocked = Float64[]
+# for bks in block_sizes
+#     A = zeros(l, n)
+#     B = randn(l, m)
+#     C = randn(m, n)
+#     push!(time_blocked, @elapsed add_to_A_B_times_C!(A, B, C, bks))
+# end
+# pl_blocked = scatter(block_sizes, time_blocked, label="blocked", axis=(xlabel="block size", ylabel="time", yscale=log10))
+# axislegend(position=:lt)
+# save("performance_blocked.pdf", pl_blocked)
+# #----------------------------------------
 
-#----------------------------------------
-# Problem d
-#----------------------------------------
-# Testing matmul function
-A = zeros(2014, 301)
-B = randn(2014, 1037)
-C = randn(1037, 301)
-println("Testing for memory allocation of oblivious matmul...")
-allocated_memory = @ballocated oblivious_add_to_A_B_times_C!(A, B, C, 64)
-@assert allocated_memory == 0
-println("No memory allocated, good!")
-println("Testing correctness of oblivious matmul...")
-A .= 0; oblivious_add_to_A_B_times_C!(A, B, C, 64)
-@assert A ≈ B * C
-println("Result correct, good!")
-#----------------------------------------
+# #----------------------------------------
+# # Problem d
+# #----------------------------------------
+# # Testing matmul function
+# A = zeros(2014, 301)
+# B = randn(2014, 1037)
+# C = randn(1037, 301)
+# println("Testing for memory allocation of oblivious matmul...")
+# allocated_memory = @ballocated oblivious_add_to_A_B_times_C!(A, B, C, 64)
+# @assert allocated_memory == 0
+# println("No memory allocated, good!")
+# println("Testing correctness of oblivious matmul...")
+# A .= 0; oblivious_add_to_A_B_times_C!(A, B, C, 64)
+# @assert A ≈ B * C
+# println("Result correct, good!")
+# #----------------------------------------
 
-#----------------------------------------
-# Problem e
-#----------------------------------------
-time_oblivious = Float64[]
-for bks in block_sizes
-    A = zeros(l, n)
-    B = randn(l, m)
-    C = randn(m, n)
-    push!(time_oblivious, @elapsed oblivious_add_to_A_B_times_C!(A, B, C, bks))
-end
-scatter!(block_sizes, time_oblivious, label="oblivious")
-axislegend(position=:lt)
-save("performance_blocked_and_oblivious.pdf", pl_blocked)
-#----------------------------------------
+# #----------------------------------------
+# # Problem e
+# #----------------------------------------
+# time_oblivious = Float64[]
+# for bks in block_sizes
+#     A = zeros(l, n)
+#     B = randn(l, m)
+#     C = randn(m, n)
+#     push!(time_oblivious, @elapsed oblivious_add_to_A_B_times_C!(A, B, C, bks))
+# end
+# scatter!(block_sizes, time_oblivious, label="oblivious")
+# axislegend(position=:lt)
+# save("performance_blocked_and_oblivious.pdf", pl_blocked)
+# #----------------------------------------
--- HW2_code/HW2_your_code.jl
+++ HW2_code/HW2_your_code.jl
@@ -15,6 +15,19 @@ end
 # It uses blocking into blocks of size bks
 # Make sure that your function does not allocate memory
 function add_to_A_B_times_C!(A, B, C, bks)
+    @turbo for j in 1:ceil(Int, size(C, 2)/bks)
+        for k in 1:ceil(Int, size(B, 2)/bks)
+            for i in 1:ceil(Int, size(A, 1)/bks)
+                for block_j in Int((j-1)*bks):min(Int((j)*bks-1), size(C, 2)-1)
+                    for block_k in Int((k-1)*bks):min(Int((k)*bks-1), size(B,2)-1)
+                        for block_i in Int((i-1)*bks):min(Int((i)*bks-1), size(A,1)-1)
+                            A[block_i+1, block_j+1] += B[block_i+1, block_k+1] * C[block_k+1, block_j+1]
+                        end
+                    end
+                end
+            end
+        end
+    end
 end
 
 # Implements a recursive, cache oblivious algorithm
@@ -24,11 +37,16 @@ function oblivious_add_to_A_B_times_C!(A, B, C, bks)
     j_size = size(C, 2)
     k_size = size(B, 2)
 
+    i_size /= 2
+    j_size /= 2
+    k_size /= 2
+
     # If we want to further subdivide
     if min(i_size, j_size, k_size) > bks
+        oblivious_add_to_A_B_times_C!(A, B, C, bks)
     #If we are ready to break the recursion
     else
-        add_to_A_B_times_C!(A, B, C)
+        add_to_A_B_times_C!(A[], B[], C[])
     end
 end
 
