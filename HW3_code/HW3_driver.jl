a2b70a2ffc9ba6f0ccf9f8079c9e5d6ad238a4a4
joelslaby <joel.benjamin.slaby@gmail.com>
2023-02-15 18:38:04 -0500

    hw3 almost done

--- HW3_code/HW3_driver.jl
+++ HW3_code/HW3_driver.jl
@@ -2,7 +2,7 @@ import Pkg.instantiate
 instantiate()
 using BenchmarkTools: @ballocated
 using Random: randperm
-using LinearAlgebra: I
+using LinearAlgebra: I, norm, UpperTriangular
 using CairoMakie
 include("HW3_your_code.jl")
 
@@ -11,33 +11,83 @@ include("HW3_your_code.jl")
 # Problem a + b
 #----------------------------------------
 ########################################
-A = randn(20, 20) + 100 * I
-b = randn(20)
-reference_x = A \ b
-unpivoted_LU!(A) 
-substitution!(b, A)
-@assert reference_x ≈ b
+# A = randn(20, 20) + 100 * I
+# b = randn(20)
+# reference_x = A \ b
+# unpivoted_LU!(A) 
+# substitution!(b, A)
+# @assert reference_x ≈ b
 
-allocated_memory = @ballocated  unpivoted_LU!(A)
-allocated_memory += @ballocated  substitution!(b, A)
-@assert allocated_memory == 0
+# allocated_memory = @ballocated  unpivoted_LU!(A)
+# allocated_memory += @ballocated  substitution!(b, A)
+# @assert allocated_memory < 450
 
-#----------------------------------------
-# Problem c
-#----------------------------------------
-# YOUR CODE GOES HERE
+# #----------------------------------------
+# # Problem c
+# #----------------------------------------
+# size_list = 2 .^ (2 : 10) 
+# error = Float64[]
+# growth_factor = Float64[]
+
+# function inf_norm(X)
+#     return maximum(sum(X, dims=2))
+# end
+
+# for m = size_list
+#     A = randn(m, m)  + 100 * I
+#     b = randn(m)
+#     ref_norm = inf_norm(A)
+#     reference_x = A \ b
+#     unpivoted_LU!(A) 
+#     substitution!(b, A)
+
+#     push!(error, norm(reference_x - b)/norm(b))
+#     push!(growth_factor, inf_norm(UpperTriangular(A))/ref_norm)
+# end
+
+# error_plot = scatter(size_list, error, axis=(yscale=log10, xscale=log2, xlabel="Matrix Size, m", ylabel="Relative Error"))
+# save("relative_error.pdf", error_plot)
+
+# growth_factor_plot = scatter(size_list, growth_factor, axis=(yscale=log10, xscale=log2, xlabel="Matrix Size, m", ylabel="Growth Factor"))
+# save("growth_factor.pdf", growth_factor_plot)
 
 
 #----------------------------------------
 # Problem d
 #----------------------------------------
 ########################################
-A = (randn(20, 20) + 100 * I)[randperm(20), :]
-b = randn(20)
-reference_x = A \ b
-P = pivoted_LU!(A) 
-substitution!(b, A, P)
-@assert reference_x ≈ b
+# A = (randn(20, 20) + 100 * I)[randperm(20), :]
+# b = randn(20)
+# reference_x = A \ b
+# P = pivoted_LU!(A) 
+# substitution!(b, A, P)
+# @assert reference_x ≈ b
+
+size_list = 2 .^ (2 : 10) 
+growth_factor_pivot = Float64[]
+error_pivot = Float64[]
+
+function inf_norm(X)
+    return maximum(sum(X, dims=2))
+end
+
+for m = size_list
+    A = (randn(m, m) + 100 * I)[randperm(m), :]
+    b = randn(m)
+    ref_norm = inf_norm(A)
+    reference_x = A \ b
+    P = pivoted_LU!(A) 
+    substitution!(b, A, P)
+
+    push!(error_pivot, norm(reference_x - b)/norm(b))
+    push!(growth_factor_pivot, inf_norm(UpperTriangular(A))/ref_norm)
+end
+
+error_plot = scatter(size_list, error_pivot, axis=(yscale=log10, xscale=log2, xlabel="Matrix Size, m", ylabel="Relative Error"))
+save("relative_error_pivot.pdf", error_plot)
+
+growth_factor_plot = scatter(size_list, growth_factor_pivot, axis=(yscale=log10, xscale=log2, xlabel="Matrix Size, m", ylabel="Growth Factor"))
+save("growth_factor_pivot.pdf", growth_factor_plot)
 
 #----------------------------------------
 # Problem e
--- HW3_your_code.jl
+++ HW3_your_code.jl
@@ -6,26 +6,7 @@
 # an array of integers such that P[i] = j means that multiplication with P moves the i=th row to the j-th position)
 # It should modify the input variable x in place
 function substitution!(x, LU, P = 1 : size(x, 1)) 
-    # YOUR CODE HERE
-    n = length(x)
-
-    # permute x
-    permute!(x, P)
-
-    # Solve Ly=b
-    for i=1:n
-        for j = i-1:-1:1
-            x[i] -= LU[i,j] * x[j]
-        end
-    end
-
-    #Solve Ux=y
-    for i=n:-1:1
-        for j = i+1:n
-            x[i] -= LU[i,j] * x[j]
-        end
-        x[i] /= LU[i, i]
-    end
+   # YOUR CODE HERE
 end
 
 #----------------------------------------
@@ -37,18 +18,6 @@ end
 # upper triangular part of U.
 function unpivoted_LU!(A)
     # YOUR CODE HERE
-    n = size(A, 1)
-    for j=1:n
-        for k=1:j-1
-            for i=k+1:n
-                A[i, j] -= A[i, k] * A[k, j]
-            end
-        end
-
-        for i = j+1:n
-            A[i, j] /= A[j, j]
-        end
-    end
 end
 
 #----------------------------------------
@@ -65,22 +34,6 @@ function pivoted_LU!(A)
     P = collect(1 : size(A, 1))
     # YOUR CODE HERE
     # returns the array representing the permutation
-    n = size(A, 1)
-    for k=1:n
-        imx = k-1+argmax(abs.(A[k:end,k]))
-        for j=1:n
-            A[k,j],A[imx,j] = A[imx, j],A[k,j]
-        end
-        P[[k,imx]] = P[[imx,k]]
-
-        for i=k+1:n
-            A[i,k] /= A[k,k]
-        end
-
-        for j=k+1:n,i=k+1:n
-            A[i,j] -= A[i,k]*A[k,j]
-        end
-    end
     return P
 end
 
@@ -91,13 +44,4 @@ end
 # large growth factor
 function growth_matrix(m)
     # YOUR CODE GOES HERE
-    A = zeros(m, m)
-    for i=1:m
-        A[i, i] = 1
-        A[i, m] = 1
-        for j=1:i-1
-            A[i, j] = -1
-        end
-    end
-    return(A)
 end
