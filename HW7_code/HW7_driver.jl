8e3775295f2044f2707608bd588ebe6fa59290d7
joelslaby <joel.benjamin.slaby@gmail.com>
2023-04-25 10:26:20 -0400

    hw7 final

--- HW7_code/HW7_driver.jl
+++ HW7_code/HW7_driver.jl
@@ -35,5 +35,45 @@ println("Passed part (b) test")
 #----------------------------------------
 ########################################
 
-# Your code here
+# Compare Arnoldi to Lanczos
+m = 8
+A = rand(m,m)
+Q,_ = qr(A)
+λ = 3 .^ range(0,m-1)
+Σ = diagm(λ)
+T = Q'Σ*Q
 
+max_eigs_A = zeros(m)
+max_eigs_L = zeros(m)
+
+# display(T)
+for kmax = 1:m
+    H_A = arnoldi(T, randn(m), kmax)
+    max_eigs_A[kmax] = sort(eigvals(H_A), rev=true)[1]
+
+    a, b = lanczos(T, randn(m), kmax)
+    H_L = diagm(-1 => b, 0 => a, 1 => b)
+    max_eigs_L[kmax] = sort(eigvals(H_L), rev=true)[1]
+end
+
+max_eig_A_error = abs.(max_eigs_A .- 3^(m-1))/3^(m-1)
+max_eig_L_error = abs.(max_eigs_L .- 3^(m-1))/3^(m-1)
+
+error_plot = scatter(1:m, max_eig_A_error, label="Arnoldi", axis=(yscale=log10, xlabel="iteration, k", ylabel="Normalized Eigenvalue Error"))
+scatter!(1:m, max_eig_L_error, label="Lanczos")
+axislegend(position=:rt)
+save("compare.pdf", error_plot)
+
+
+# Create ghost eigenvalues
+m = 8
+A = rand(m,m)
+Q,_ = qr(A)
+λ = [1, 2, 3, 4, 5, 6, 7, 1e5]
+Σ = diagm(λ)
+T = Q'Σ*Q
+
+a, b = lanczos(T, randn(m), m)
+H_L = diagm(-1 => b, 0 => a, 1 => b)
+display(eigvals(H_L))
+display(eigvals(T))
--- HW7_code/HW7_your_code.jl
+++ HW7_code/HW7_your_code.jl
@@ -7,10 +7,29 @@
 # The matrix A should be only accessed through 
 # (kmax - 1) matrix-vector products
 function arnoldi(A, q1, kmax)
-        
-        # Your code here
+    H = zeros(kmax, kmax)
+    Q = zeros(size(q1, 1), kmax)
 
-        return H
+    Q[:, 1] = q1 / norm(q1)
+    for k = 1:kmax
+        if k > 1
+            Q[:, k] = q1/ H[k, k-1]
+        end
+
+        q1 = A * Q[:, k]
+        for i = 1:k
+            H[i, k] = 0
+            for j = 1:size(q1, 1)
+                H[i, k] += Q[j, i] * q1[j]
+            end
+            q1 -= H[i, k] * Q[:, i]
+        end
+
+        if k < kmax
+            H[k+1, k] = norm(q1)
+        end
+    end
+    return H
 end
 
 #----------------------------------------
@@ -24,8 +43,26 @@ end
 # The output vectors should be the diagonal (α)
 # and the offdiagonal (β) of the tridiagonal matrix
 function lanczos(A, q1, kmax)
+    T = zeros(kmax, kmax)
+    r = copy(q1[:, 1])
+    beta = norm(r)
+    q1 = zeros(size(A, 1))
 
-        # Your code here
-
-        return α, β
+    for k = 1:kmax
+        q0 = copy(q1)
+        q1 = r/beta
+        r = A * q1
+        alpha = 0
+        for j = 1:size(r, 1)
+            alpha += q1[j] * r[j]
+        end
+        T[k, k] = alpha
+        if k > 1
+            T[k-1, k] = beta
+            T[k, k-1] = beta
+        end
+        r = r - alpha*q1 - beta * q0
+        beta = norm(r)
+    end
+    return diag(T), diag(T,1)
 end
